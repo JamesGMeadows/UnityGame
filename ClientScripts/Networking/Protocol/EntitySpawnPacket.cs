3f6e122f181a704720620041ea6e716de1f40b82
James Meadows <77756533+JamesGMeadows@users.noreply.github.com>
2022-03-22 14:20:34 -0500

    Re-Added Client Scripts

---
+++ ClientScripts/Networking/Protocol/EntitySpawnPacket.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Networking.Protocol
+{
+    public class EntitySpawnPacket : Packet
+    {
+        int id, type;
+        float x, y, z;
+        public EntitySpawnPacket(int id, int type, float x, float y, float z) : base(PacketType.ENTITY_SPAWN_PACKET)
+        {
+            this.id = id;
+            this.type = type;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public EntitySpawnPacket(int id, int type, Vector3 vec) : this(id, type, vec.x, vec.y, vec.z)
+        {
+
+        }
+
+        public EntitySpawnPacket() : this(-1, -1, 0, 0, 0)
+        {
+
+        }
+
+        public float GetX()
+        {
+            return x;
+        }
+
+        public float GetY()
+        {
+            return y;
+        }
+
+        public float GetZ()
+        {
+            return z;
+        }
+
+        public int GetNetworkId()
+        {
+            return id;
+        }
+
+        public int GetEntityType()
+        {
+            return type;
+        }
+
+        public override void Decode(byte[] array, PacketDecoder decoder)
+        {
+            id = decoder.NextInt(array);
+            type = decoder.NextInt(array);
+            x = decoder.NextFloat(array);
+            y = decoder.NextFloat(array);
+            z = decoder.NextFloat(array);
+        }
+
+        public override void Encode(BinaryWriter writer)
+        {
+            writer.Write(id);
+            writer.Write(type);
+            writer.Write(x);
+            writer.Write(y);
+            writer.Write(z);
+        }
+    }
+}
---
+++ ClientScripts/PlayerEquipment.cs
@@ -0,0 +1,47 @@
+﻿using Assets.Scripts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEquipment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject equippedPrefab;
+    public GameObject hand;
+
+    private Animator animator;
+    private PlayerController controller;
+
+    private Timer updateTimer;
+    private GameObject equipped;
+    public bool IsHitting = false;
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+        var pos = hand.transform.position; //+ new Vector3(0, 2.3f, 0) + transform.forward * 2;
+        equipped = Instantiate(equippedPrefab, pos, Quaternion.identity) as GameObject;
+        equipped.transform.SetParent(hand.transform);
+        animator = GetComponentInChildren<Animator>();
+
+        updateTimer = new Timer(.151f);
+    }
+
+    // Update is called once per frame
+    Vector3 originalPos;
+    void Update()
+    {
+
+        if (updateTimer.CheckFinished(false))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                updateTimer.NextTime();
+                animator.SetTrigger("Hit");
+                IsHitting = true;
+            }
+
+        }
+    }
+
+}
